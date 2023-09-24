ea3829d7216178c4f4037c00537b51556e40b3f6
jeomin12 <jeomin.gimmi@my.jcu.edu.au>
2023-09-24 22:50:29 +1000

    Add user and random score evaluation

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,17 +4,19 @@ Broken program to determine score status
 """
 
 import random
-
 def main():
+  """Get user score, generate random score and print both results."""
+
   user_score = float(input("Enter your score: "))
   result = get_score_result(user_score)
   print(result)
 
   random_score = random.randint(0, 100)
-  print(f"Random score: {random_score}")
+  print(f"Random score: {random_score}")  # Generate a random score
   print(get_score_result(random_score))
 
 def get_score_result(score):
+  """Determine result based on the score."""
   if score < 0 or score > 100:
     return "Invalid score"
   elif score < 50:
