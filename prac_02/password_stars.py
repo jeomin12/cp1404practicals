935e95fd55623340606d42cf9243a0aed6141b01
jeomin12 <jeomin.gimmi@my.jcu.edu.au>
2023-09-24 22:49:29 +1000

    Refactor password validation and printing

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,17 +1,18 @@
 MIN_LENGTH = 8
 def main():
+    """Prompt and print the password"""
     password = input("Enter password: ")
-
     password = get_password(password)
-
-    print("*" * len(password))
-
+    print_asterics(password)
 
 def get_password(password):
+    """Checks password meets minimum length requirement."""
     while len(password) < MIN_LENGTH:
         print("Password too short! Try again.")
         password = input("Enter password (minimum {} characters): ".format(MIN_LENGTH))
     return password
-
+def print_asterics(password):
+    """Print asteriks to the password."""
+    print("*" * len(password))
 
 main()
