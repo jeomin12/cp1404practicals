d315a7a68b721a3aeb24a2fcd43616109500cd38
jeomin12 <jeomin.gimmi@my.jcu.edu.au>
2023-10-01 22:46:53 +1000

    Added random number generation

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+# print(random.randint(5, 20))
+# Smallest number: 5
+# Largest number: 20
+
+# print(random.randrange(3, 10, 2))
+# Smallest number: 3
+# Largest number: 9
+# No, line 2 could not produce a 4 since it only produces odd numbers.
+
+
+# print(random.uniform(2.5, 5.5))
+# Smallest number: 2.5
+# Largest number: Number slightly less than 5.5
+
+import random
+print(random.randint(1, 100))
