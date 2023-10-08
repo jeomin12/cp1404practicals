1cf32ae25aaf724b99295ac0676a5f8d803c13d3
jeomin12 <jeomin.gimmi@my.jcu.edu.au>
2023-10-08 21:30:16 +1000

    Refactor program into functions

---
+++ prac_04/total_income.py
@@ -0,0 +1,30 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        # Get income for each month and add it to list
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
+    """ Prints income report. """
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, number_of_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print(f"Month {month:2} - Income: ${income:10.2f}      Total: ${total:10.2f}")
+
+
+main()
