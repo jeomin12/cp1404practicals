5d377a2ea8ff9bda4e629e0e93a5c34e4d6f2b99
jeomin12 <jeomin.gimmi@my.jcu.edu.au>
2023-10-14 22:20:35 +1000

    Updated exception handling

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,18 @@ State names in a dictionary
 File needs reformatting
 """
 
-
-CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
+max_len = len(max(CODE_TO_NAME, key=len))
+for code in CODE_TO_NAME:
+    print("{:<{}} is {}".format(code, max_len, CODE_TO_NAME[code]))
