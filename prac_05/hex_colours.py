9697cceb77ee65095ecbdd4ac1b198cf6ba1ab09
jeomin12 <jeomin.gimmi@my.jcu.edu.au>
2023-10-15 22:40:49 +1000

    Added strip() to the code

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,11 +1,12 @@
-COLORS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "Aqua": "#00ffff", "Aquamarine": "#7fffd4",
-          "Azure": "#f0ffff", "Beige": "#f5f5dc", "Bisque": "#ffe4c4", "Black": "#000000", "BlanchedAlmond": "#ffebcd",
+COLORS = {"Alizarin Crimson": "#f0f8ff", "Amber": "#faebd7", "Aqua": "#00ffff", "Aquamarine": "#7fffd4",
+          "Azure": "#f0ffff", "Beige": "#f5f5dc", "Bisque": "#ffe4c4", "Black": "#000000", "Bitter Lemon": "#ffebcd",
           "Blue": "#0000ff"}
 
-color = input("Enter a color name: ").lower()
+color = input("Enter a color name: ").title().strip()
 while color != "":
-    try:
-        print(color, "is", COLORS[color])
-    except KeyError:
+    if color in COLORS:
+        print(f"{color} is  {COLORS[color]}")
+    else:
         print("Invalid color name")
-    color = input("Enter a color name: ").lower()
+
+    color = input("Enter a color name: ").title().strip()
