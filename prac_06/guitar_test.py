c8b94921478532115d51ad8e038b45e1758b1d9c
jeomin12 <jeomin.gimmi@my.jcu.edu.au>
2023-11-12 12:58:06 +1000

    Added better variable names

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,11 +3,11 @@
 """
 from guitar import Guitar
 
-g1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
-g2 = Guitar("Another Guitar", 2013, 9999.99)
+guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+guitar2 = Guitar("Another Guitar", 2013, 9999.99)
 
-print(f"{g1.name} get_age() - Expected 100. Got {g1.get_age()}")
-print(f"{g2.name} get_age() - Expected 9. Got {g2.get_age()}")
+print(f"{guitar1.name} get_age() - Expected 100. Got {guitar1.get_age()}")
+print(f"{guitar2.name} get_age() - Expected 9. Got {guitar2.get_age()}")
 
-print(f"{g1.name} is_vintage() - Expected True. Got {g1.is_vintage()}")
-print(f"{g2.name} is_vintage() - Expected False. Got {g2.is_vintage()}")
+print(f"{guitar1.name} is_vintage() - Expected True. Got {guitar1.is_vintage()}")
+print(f"{guitar2.name} is_vintage() - Expected False. Got {guitar2.is_vintage()}")
