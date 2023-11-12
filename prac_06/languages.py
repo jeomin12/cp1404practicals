3471cac01aedb65b6137cd4f738c830affc08c5b
jeomin12 <jeomin.gimmi@my.jcu.edu.au>
2023-11-12 13:00:14 +1000

    Added function docstring

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,6 +5,7 @@ from programming_language import ProgrammingLanguage
 
 
 def main():
+    """ Main function for the languages program."""
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
